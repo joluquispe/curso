7948212b7d70beb7818c5e86c175e0f28d861d85
Jolu <joluquispe2@gmail.com>
2022-12-03 22:18:16 -0500

    esto es el commit real despues muchos errores

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CURSO GIT </title>
+</head>
+<body>
+    <h1>ESTO ES MI CURSO</h1>
+     <p>esto es el primer parrafo de lo que estamos haciendo </p>
+</body>
+</html>
